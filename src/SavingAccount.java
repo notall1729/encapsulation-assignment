079f983a6945add8a1ff1b2da1a95f2a602895e2
goli <shirafkanzeynab63@gmail.com>
2025-03-15 15:16:15 +0330

    add SavingAccount methods

--- src/SavingAccount.java
+++ src/SavingAccount.java
@@ -2,10 +2,11 @@ class SavingAccount extends BankAccount{
     private final double interestRate = 0.03;
 
     public SavingAccount(String accountNumber, String accountHolderName, double initialBalance){
-
+      super(accountNumber, accountHolderName, initialBalance);
     }
 
+    @Override
     public void calculateInterest(){
-
+       deposit(interestRate * balance);
     }
 }
