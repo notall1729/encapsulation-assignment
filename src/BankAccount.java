44edb67e8c77eb4465f4ec80e3378e58de74f97f
goli <shirafkanzeynab63@gmail.com>
2025-03-15 15:09:51 +0330

    continue BankAccount code

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,3 +1,4 @@
+import java.lang.invoke.LambdaMetafactory;
 import java.util.ArrayList;
 
 public abstract class BankAccount {
@@ -7,17 +8,29 @@ public abstract class BankAccount {
     protected double balance;
 
     public BankAccount(String accountNumber, String accountHolderName, double balance){
-
+      this.accountNumber = accountNumber;
+      this.accountHolderName = accountHolderName;
+      this.balance = balance;
     }
 
     public abstract void calculateInterest();
 
     public void deposit(double amount){
-
+      if(amount > 0) {
+          amount += balance;
+          System.out.println("The amount of " + amount + "add to account number " + accountNumber);
+      }
+      else System.out.println("Amount must be positive.");
     }
 
     public void withdraw(double amount){
-
+      if(amount > 0 && amount <= balance){
+          amount -= balance;
+          System.out.println("The amount of " + amount + "deducte from the account.");
+      }
+      if(amount > 0 && amount > balance)
+          System.out.println("You can not withdraw more than your balance. you balance is: " + balance);
+      else System.out.println("Amount must be positive.");
     }
 
     public double getBalance() {
