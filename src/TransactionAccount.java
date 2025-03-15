6357cdd63550b10596cdfd71dd66ed724cbdc448
goli <shirafkanzeynab63@gmail.com>
2025-03-15 15:25:58 +0330

    add TransactionAccount methods

--- src/TransactionAccount.java
+++ src/TransactionAccount.java
@@ -1,16 +1,24 @@
 class TransactionAccount extends BankAccount{
-    private final double overDraftLimit;
+    private final double overDraftLimit = 500;
 
     public TransactionAccount(String accountHolderName, String accountNumber, double initialBalance){
-
+     super(accountNumber, accountHolderName, initialBalance);
     }
 
+    @Override
     public void calculateInterest(){
-
+     System.out.println("There is no interest on this account.");
     }
 
+    @Override
     public void withdraw(double amount){
-
+        if(amount > 0 && amount <= (balance + overDraftLimit)){
+            amount -= balance;
+            System.out.println("The amount of " + amount + "deducte from the account.");
+        }
+        if(amount > 0 && amount > (balance + overDraftLimit))
+            System.out.println("You can not withdraw more than your balance + 500 . you balance is: " + balance);
+        else System.out.println("Amount must be positive.");
     }
 }
 
